9be1cfdf2514c2311ab52100b3f0126dd6dbe5e3
rgmanikandan07 <rgmanikandan07@gmail.com>
2025-01-14 09:22:27 +0530

    node-basics

---
+++ authentication.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+    if (req.headers["api-key"] === "my-secret-key") {
+        next(); 
+    } else {
+        res.status(403).send("Forbidden"); 
+    }
+});
+
+// // Middleware
+// app.get("/", (req, res) => {
+//     res.send("Access granted!");
+// });
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
