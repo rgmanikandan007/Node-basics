c010e13198e3a8c8bb9f8b7dd7da4afbec1bd5a8
rgmanikandan07 <rgmanikandan07@gmail.com>
2025-01-14 07:58:23 +0530

    node-basics

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const http = require("http");
+// const http = require("http");
 
-const server = http.createServer((req, res)=>{
-    res.statusCode = 200
-    res.setHeader("content-type", "text/plain");
-    res.end("Hello world\n");
-})
+// const server = http.createServer((req, res)=>{
+//     res.statusCode = 200
+//     res.setHeader("content-type", "text/plain");
+//     res.end("Hello world\n");
+// })
 
-server.listen(5000,()=>{
-    console.log("server is running http://localhost:5000");
-})
+// server.listen(5000,()=>{
+//     console.log("server is running http://localhost:5000");
+// })
 
 // Create server with express 
 
-// const express = require("express");
-// const app = express();
+const express = require("express");
+const app = express();
 
-// app.get("/", (req, res)=>{
-//     res.send("Vetriii");
-// })
+app.get("/", (req, res)=>{
+    res.send("Vetriii");
+})
 
-// app.listen(5000, () => {
-//     console.log("Server running http://localhost:5000")
-// })
+app.listen(5000, () => {
+    console.log("Server running http://localhost:5000")
+})
